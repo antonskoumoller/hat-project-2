71c6a0f59aec4c4fb6195383474b5e536da9218d
emilHojgaard <emilhn94@gmail.com>
2025-04-03 13:54:20 +0200

    started marking customer reqs

--- server.js
+++ server.js
@@ -13,6 +13,40 @@ const db = new sqlite3.Database("./db/database.db", (err) => {
 
 app.use(express.json());
 
+// Endpoint to get all customers
+app.get("/customers", (req, res) => {
+	const query = "SELECT * FROM customers";
+	db.all(query, (err, row) => {
+		if (err) {
+			console.error("Error retrieving user:", err.message);
+			return res.status(500).json({ error: "Internal server error" });
+		}
+		if (!row) {
+			return res.status(404).json({ error: "User not found" });
+		}
+		res.status(200).json(row);
+	});
+});
+
+//Endpoint to create a new customer
+app.post("/customers", (req, res) => {
+	const { name, email, password } = req.body;
+	// Validate input
+	if (!name || !email || !password) {
+		return res.status(400).json({ error: "Name, email, and password are required" });
+	}
+	// querry 
+	const query = "INSERT INTO customers (name, email, password) VALUES (?, ?, ?)";
+
+	db.post(query, [name, email, password], function (err) {
+		if (err) {
+			console.error("Error creating user:", err.message);
+			return res.status(500).json({ error: "Internal server error" });
+		}
+		res.status(201).json({ id: this.lastID, name, email });
+	});
+})
+
 // Endpoint to get customer with id
 app.get("/customers/:id", (req, res) => {
 	const customerId = req.params.id;
@@ -29,20 +63,11 @@ app.get("/customers/:id", (req, res) => {
 	});
 });
 
-// Endpoint to get customers
-app.get("/customers", (req, res) => {
-	const query = "SELECT * FROM customers";
-	db.all(query, (err, row) => {
-		if (err) {
-			console.error("Error retrieving user:", err.message);
-			return res.status(500).json({ error: "Internal server error" });
-		}
-		if (!row) {
-			return res.status(404).json({ error: "User not found" });
-		}
-		res.status(200).json(row);
-	});
-});
+// Update customer with id
+
+//Delete customer with id
+
+
 
 // GET /customers/:id/basket
 app.get("/customers/:id/basket", (req, res) => {
