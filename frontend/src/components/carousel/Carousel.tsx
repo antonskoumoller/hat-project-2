90a1051de4a32ba96e58f9a8f078ca905e08a2e2
AnyChristensen <anne.ny.christensen@gmail.com>
2025-05-18 11:15:39 +0200

    cleaned carousel.tsx

--- frontend/src/components/carousel/Carousel.tsx
+++ frontend/src/components/carousel/Carousel.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import ItemCard, { HatItem } from "../ItemCard";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
-import { ShowProductOverlay } from "../productOverlay/ProductOverlay";
 
 // export interface CarouselProps
 type CarouselProps = {
@@ -13,52 +12,39 @@ export default function Carousel({
 	CarouselHats,
 	hatsPerSlide
 }: CarouselProps) {
-	// State holding the index for the current slide
-	const [currentSlide, setCurrentSlide] = React.useState(0);
+	// useStates, one holding the index for the current slide and one holding the state of overlay
+	const [currentSlide, setCurrentSlide] = useState(0);
+	const [overlayOpen, setOverlayOpen] = useState(false);
 
-	// Finding the number of slides (ceil rounds up)
+	// Creating slides for the carousel
 	const numOfSlides = Math.ceil(CarouselHats.length / hatsPerSlide);
-
-	// Slicing the array of hats into slides
 	const startHat = currentSlide * hatsPerSlide;
 	const currentHats = CarouselHats.slice(startHat, startHat + hatsPerSlide);
 
-	// Setting up a timer to change slide every 7. second
-	React.useEffect(() => {
-		const interval = setInterval(() => {
-			if (!ShowProductOverlay) {
-				nextSlide();
-			}
-		}, 7000);
-
-		// Clearing the timer when the component rerenders
-		return () => clearInterval(interval);
-	}, [currentSlide]);
-
-	// navigate to previous slide
-	function prevSlide() {
-		setCurrentSlide((prevSlide) =>
-			prevSlide <= 0 ? numOfSlides - 1 : prevSlide - 1
-		);
-	}
-
-	// navigate to next slide
+	// Navigation next slide
 	function nextSlide() {
 		setCurrentSlide((prevSlide) =>
 			prevSlide >= numOfSlides - 1 ? 0 : prevSlide + 1
 		);
 	}
 
-	console.log(
-		"Index for current slide:",
-		currentSlide,
-		"Start hat:",
-		startHat,
-		"CurrentHats:",
-		currentHats,
-		"CarouselHats length:",
-		CarouselHats.length
-	);
+	// Navigation previous slide
+	function prevSlide() {
+		setCurrentSlide((prevSlide) =>
+			prevSlide <= 0 ? numOfSlides - 1 : prevSlide - 1
+		);
+	}
+
+	// Setting up a timer to change slide every 7. second
+	useEffect(() => {
+		if (overlayOpen) return;
+		const timer = setInterval(() => {
+			nextSlide();
+		}, 7000);
+
+		// Clearing the timer when the component rerenders
+		return () => clearInterval(timer);
+	}, [currentSlide, overlayOpen]);
 
 	return (
 		<div>
@@ -73,10 +59,16 @@ export default function Carousel({
 
 			{/* Navigation Buttons (styling fælles div med buttons nede)px-4 py-2 bg-gray-200 rounded hover:bg-gray-300 */}
 			<div className="absolute left-0 top-1/2 transform -translate-y-1/2 p-2">
-				<SlArrowLeft onClick={prevSlide} />
+				<SlArrowLeft
+					onClick={prevSlide}
+					className="text-teal-500 w-6 h-6 hover:text-teal-200"
+				/>
 			</div>
 			<div className="absolute right-0 top-1/2 transform -translate-y-1/2 p-2">
-				<SlArrowRight onClick={nextSlide} />
+				<SlArrowRight
+					onClick={nextSlide}
+					className="text-teal-500 w-6 h-6 hover:text-teal-200"
+				/>
 			</div>
 		</div>
 	);
